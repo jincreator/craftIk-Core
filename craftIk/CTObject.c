4ba4f351314d398599a4216e944e01cdfd63d7ee
Cyrus I. Hackford <cihackford@cyrusian.com>
2012-05-05 17:18:04 +0900

    Made CTObject's methods more multithread-friendly...?

--- craftIk/CTObject.c
+++ craftIk/CTObject.c
@@ -7,10 +7,13 @@
 //
 
 #include "CTObject.h"
+#include <pthread.h>
 
 
 struct _CTObjectHeader {
 	CTShort referenceCount;
+	pthread_mutex_t *lock;
+	pthread_mutexattr_t *lockAttributes;
 };
 
 struct _CTObjectTemplate {
@@ -29,7 +32,11 @@ CTBOOL CTAllocate(void **objectPosition, size_t objectSize) {
 	
 	CTObjectHeader *rawHeader=(CTObjectHeader *)rawMemory;
 	struct _CTObjectTemplate *rawObject=(struct _CTObjectTemplate *)(rawMemory+sizeof(CTObjectHeader));
+	
 	rawHeader->referenceCount=1;
+	pthread_mutexattr_init(rawHeader->lockAttributes);
+	pthread_mutexattr_settype(rawHeader->lockAttributes, PTHREAD_MUTEX_ERRORCHECK);
+	pthread_mutex_init(rawHeader->lock, rawHeader->lockAttributes);
 	rawObject->header=rawHeader;
 	
 	*objectPosition=rawObject;
@@ -38,16 +45,25 @@ CTBOOL CTAllocate(void **objectPosition, size_t objectSize) {
 }
 
 void CTRelease(void *object, void (*objectCleanUpFunction)(void *)) {
-	struct _CTObjectTemplate *stdObj=(struct _CTObjectTemplate *)object;
+	struct _CTObjectTemplate *standardObject=(struct _CTObjectTemplate *)object;
 	
-	--(stdObj->header->referenceCount);
-	if(stdObj->header->referenceCount==0) {
+	pthread_mutex_lock(standardObject->header->lock);
+	--(standardObject->header->referenceCount);
+	if(standardObject->header->referenceCount==0) {
 		if(objectCleanUpFunction!=NULL)
 			objectCleanUpFunction(object);
-		free(stdObj->header);
-	}
+		pthread_mutex_unlock(standardObject->header->lock);
+		pthread_mutex_destroy(standardObject->header->lock);
+		pthread_mutexattr_destroy(standardObject->header->lockAttributes);
+		free(standardObject->header);
+	} else
+		pthread_mutex_unlock(standardObject->header->lock);
 }
 
 void CTRetain(void *object) {
-	++(((struct _CTObjectTemplate *)object)->header->referenceCount);
+	struct _CTObjectTemplate *standardObject=(struct _CTObjectTemplate *)object;
+	
+	pthread_mutex_lock(standardObject->header->lock);
+	++(standardObject->header->referenceCount);
+	pthread_mutex_unlock(standardObject->header->lock);
 }
