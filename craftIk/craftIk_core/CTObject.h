0073f752a50b59eead044266f7a3ad8cf6c7b7f2
Jinkyu Yi <jincreator@jincreator.net>
2012-05-29 17:41:03 +0900

    Add legal notice + etc...

--- craftIk/craftIk_core/CTObject.h
+++ craftIk/craftIk_core/CTObject.h
@@ -1,10 +1,21 @@
-//
-//  CTObject.h
-//  craftIk-Core
-//
-//  Created by Cyrus Hackford on 5/29/12.
-//  Copyright (c) 2012 SI Cyrusian. All rights reserved.
-//
+/*
+	craftIk - A custom Minecraft server written in C.
+	Copyright (C) 2012, SI Cyrusian. All Rights Reserved.
+	Minecraft is a trademark of Mojang AB.
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef _CTOBJECT_H_
 #define _CTOBJECT_H_
